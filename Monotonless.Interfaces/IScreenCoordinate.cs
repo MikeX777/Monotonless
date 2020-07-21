0bcdd2cec0257aa693546b7ba772c6c39ec66639
BABY_PLS\Michael Cuomo <mcuomo777@gmail.com>
2020-07-20 21:09:49 -0400

    Robot Interfaces

---
+++ Monotonless.Interfaces/IScreenCoordinate.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monotonless.Interfaces
+{
+    public interface IScreenCoordinate
+    {
+    }
+}
---
+++ Monotonless.Interfaces/IWindowRobot.cs
@@ -0,0 +1,14 @@
+﻿namespace Monotonless.Interfaces
+{
+    /// <summary>
+    /// Robot for running simple window processes.
+    /// </summary>
+    public interface IWindowRobot
+    {
+        /// <summary>
+        /// Brings up the window by the supplied title.
+        /// </summary>
+        /// <param name="title">The supplied window title.</param>
+        void SetForegroundWindow(string title);
+    }
+}
